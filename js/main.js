23f50fac46cce62c7667c3aa68f45de1f623d8ff
shadyo0o <shady.etra@gmail.com>
2024-06-10 04:35:15 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,144 @@
+
+
+var warnInput =document.getElementById("warn")
+var acceptInput =document.getElementById("accept")
+var nameInput=document.getElementById("name")
+var emailInput=document.getElementById("email");
+var passInput=document.getElementById("pass");
+var supInput=document.getElementById("btnsup")
+var putInput=document.getElementById("put")
+
+
+var regex={
+    name:{
+        value:/^[a-z]{2,8}$/i,
+        Status:false,
+    },
+    email:{
+value:/^[a-zA-Z]{1,}\.(gmail|yahoo)\.com$/,
+Status:false
+    },
+    pass:{
+        value:/^[1-9]{3,8}$/,
+        status:false,
+    },
+}
+
+// ********************************* page sign up************************************
+
+function validateName(){
+    if(regex.name.value.test(nameInput.value)){
+        localStorage.setItem("name",JSON.stringify(nameInput.value))
+        regex.name.status=true
+        nameInput.classList.remove("is-invalid")
+        nameInput.classList.add("is-valid")
+        return true
+        }else{
+            regex.name.status=false
+            nameInput.classList.remove("is-valid")
+            nameInput.classList.add("is-invalid")
+            return false
+            }
+}
+
+
+
+function validateEmail(){
+    if(regex.email.value.test(emailInput.value) ){
+        regex.email.status=true
+        emailInput.classList.remove("is-invalid")
+        emailInput.classList.add("is-valid")
+        return true
+        }
+        else{
+            regex.email.status=false
+            emailInput.classList.remove("is-valid")
+            emailInput.classList.add("is-invalid")
+            return false
+            }
+}
+
+function validatePass()
+{
+    if(regex.pass.value.test(passInput.value)){
+        regex.pass.status=true
+        passInput.classList.remove("is-invalid")
+        passInput.classList.add("is-valid")
+
+        return true
+        }else{
+            regex.pass.status=false
+            passInput.classList.remove("is-valid")
+            passInput.classList.add("is-invalid")
+            return false
+            }
+}
+var container=[];
+
+function signup(){
+
+    if(validateName() && validateEmail() && validatePass()){
+
+
+        container.push({
+            name:nameInput.value,
+            email:emailInput.value,
+            pass:passInput.value,
+    })
+    console.log(container);
+    localStorage.setItem("login" ,JSON.stringify(container) )
+    acceptInput.classList.remove("d-none")
+
+        }else{
+            warnInput.classList.remove("d-none")
+            }
+
+}
+// *************************************************************
+
+// ************************ page login*************************
+
+var email1input=document.getElementById("email1");
+var pass1input=document.getElementById("pass1");
+var requiredinput=document.getElementById("req");
+
+function login(){
+    var loginData=JSON.parse(localStorage.getItem("login"));
+    console.log(loginData);
+    
+    for(var i=0;i<loginData.length;i++){
+        if(email1input.value===loginData[i].email && pass1input.value===loginData[i].pass)
+            {
+                acceptInput.classList.remove("d-none")
+                warnInput.classList.add("d-none")
+                console.log(loginData);
+
+                accept()
+                return 
+            }else if(email1input.value=="" || pass1input.value==""){
+                requiredinput.classList.remove("d-none");   
+            }
+            else{
+                warnInput.classList.remove("d-none")
+                acceptInput.classList.add("d-none")
+                requiredinput.classList.add("d-none");
+return 
+            }
+}
+
+}
+// **************************************************
+
+
+
+// **************** go to final page ********************
+function accept()
+{
+    window.location.href="page3.html";
+}
+
+
+document.getElementById("userName").innerHTML= `Welcome ${JSON.parse(localStorage.getItem("name"))}`;
+
+// *******************************************************
+
